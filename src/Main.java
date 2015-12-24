2e04592d5d5b8592ad9a4dbbd3afd55157389c11
Exitsoftware <kbk9288@gmail.com>
2015-12-24 23:44:54 +0900

    Commit
    Commit

--- src/Main.java
+++ src/Main.java
@@ -30,7 +30,7 @@ import org.w3c.dom.NodeList;
 
 public class Main {
 
-	ArrayList<String> resultString = new ArrayList<String>();
+	ArrayList<ArrayList<String>> resultString = new ArrayList<ArrayList<String>>();
 	List<Map<String, Object>> data;
 	String query;
 
@@ -38,7 +38,7 @@ public class Main {
 		try {
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
 					new FileOutputStream("Output3.txt"), "UTF8"));
-			for (String line : resultString) {
+			for (ArrayList<String> line : resultString) {
 				bw.write(line + "\n");
 				bw.flush();
 			}
@@ -46,6 +46,8 @@ public class Main {
 			System.out.println(ex);
 		}
 	}
+	
+	
 
 	public void start() {
 		try {
@@ -53,7 +55,9 @@ public class Main {
 			Scanner s = new Scanner(System.in);
 			query = s.next();
 			String address = "http://openapi.naver.com/search?key=41e7b6581f7c9d85d41271a9033527d1&query="
-					+ query + "&target=news&start=1&display=20";
+					+ query + "&target=news&start=1&display=10";
+			
+			
 //			URL url = new URL(address);
 //			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 //			conn.setRequestMethod("GET");
@@ -76,7 +80,8 @@ public class Main {
 
 			NodeList itemList = document.getElementsByTagName("item");
 			for (int i = 0; i < itemList.getLength(); i++) {
-				String line = "";
+				
+				ArrayList<String> line = new ArrayList<String>();
 
 				Element element = (Element) itemList.item(i);
 
@@ -84,41 +89,41 @@ public class Main {
 				NodeList titleList = element.getElementsByTagName("title");
 				Element titleElmnt = (Element) titleList.item(0);
 				Node title = titleElmnt.getFirstChild();
-				System.out.println(title.getNodeValue());
-				line += title.getNodeValue();
-				line += "$";
+//				System.out.println(title.getNodeValue());
+				line.add(title.getNodeValue());
+				
 
 				// 설명
 				NodeList descriptionList = element
 						.getElementsByTagName("description");
 				Element descriptionElmnt = (Element) descriptionList.item(0);
 				Node description = descriptionElmnt.getFirstChild();
-				System.out.println(description.getNodeValue());
-				line += description.getNodeValue();
-				line += "$";
+//				System.out.println(description.getNodeValue());
+				line.add(description.getNodeValue());
+				
 
 				// 날짜
 				NodeList dateList = element.getElementsByTagName("pubDate");
 				Element dateElmnt = (Element) dateList.item(0);
 				Node date = dateElmnt.getFirstChild();
-				System.out.println(date.getNodeValue());
-				line += date.getNodeValue();
-				line += "$";
+//				System.out.println(date.getNodeValue());
+				line.add(date.getNodeValue());
+				
 
 				// 링크
 				NodeList linkList = element
 						.getElementsByTagName("originallink");
 				Element linkElmnt = (Element) linkList.item(0);
 				Node link = linkElmnt.getFirstChild();
-				System.out.println(link.getNodeValue());
-				line += link.getNodeValue();
-//				line += "$";
+//				System.out.println(link.getNodeValue());
+				line.add(link.getNodeValue());
 
+				System.out.println("??"+i);
 				resultString.add(line);
 
 			}
-
-			output();
+			System.out.println("testing");
+//			output();
 			excelInput();
 			excelOutput();
 
@@ -127,10 +132,78 @@ public class Main {
 			// "UTF-8");
 			// System.out.println(isr.toString());
 		} catch (Exception ex) {
-
+			System.out.println(ex);
 		}
 	}
 
+	public void daumStart() {
+		try {
+			System.out.print("검색할 단어를 입력하세요 : ");
+			Scanner s = new Scanner(System.in);
+			query = s.next();
+			String address = "https://apis.daum.net/search/board?apikey=884e32eb590e5137ff5093107c75a9cf&q="
+					+ query + "카카오톡&output=xml";
+			
+
+			Document document = DocumentBuilderFactory.newInstance()
+					.newDocumentBuilder().parse(address);
+
+			NodeList itemList = document.getElementsByTagName("item");
+			System.out.println(itemList.getLength());
+			for (int i = 0; i < itemList.getLength(); i++) {
+				
+				ArrayList<String> line = new ArrayList<String>();
+
+				Element element = (Element) itemList.item(i);
+
+				// 기사 제목
+				NodeList titleList = element.getElementsByTagName("title");
+				Element titleElmnt = (Element) titleList.item(0);
+				Node title = titleElmnt.getFirstChild();
+//				System.out.println(title.getNodeValue());
+				line.add(title.getNodeValue());
+				
+				
+				
+				// 설명
+				NodeList descriptionList = element
+						.getElementsByTagName("description");
+				Element descriptionElmnt = (Element) descriptionList.item(0);
+				Node description = descriptionElmnt.getFirstChild();
+//				System.out.println(description.getNodeValue());
+				line.add(description.getNodeValue());
+				
+				
+				// 날짜
+				NodeList dateList = element.getElementsByTagName("pubDate");
+				Element dateElmnt = (Element) dateList.item(0);
+				Node date = dateElmnt.getFirstChild();
+				System.out.println(date.getNodeValue());
+				line.add(date.getNodeValue());
+				
+
+				// 링크
+				NodeList linkList = element
+						.getElementsByTagName("link");
+				Element linkElmnt = (Element) linkList.item(0);
+				Node link = linkElmnt.getFirstChild();
+//				System.out.println(link.getNodeValue());
+				line.add(link.getNodeValue());
+
+				System.out.println("??"+i);
+				resultString.add(line);
+
+			}
+			System.out.println("testing");
+			excelInput();
+			excelOutput();
+
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+	}
+	
+	
 	public void excelOutput() {
 		try {
 			// WorkBook 생성
@@ -138,7 +211,7 @@ public class Main {
 					"ExcelWriteSample.xls"));
 
 			// WorkSheet 생성
-			WritableSheet sh = wb.createSheet("테스트", 0);
+			WritableSheet sh = wb.createSheet("네이버", 0);
 			
 			// 열넓이 설정 (열 위치, 넓이)
 			sh.setColumnView(0, 20);
@@ -224,19 +297,20 @@ public class Main {
 		for(int i = 0; i < resultString.size(); i++){
 			Map<String, Object> map = new HashMap<String, Object>();
 			
-			String[] lines;
-			lines = resultString.get(i).split("\\$");
-			System.out.println(lines[0]);
-			for(String line : lines){
-				System.out.println(line);
-			}
-			System.out.println("lines size : " + lines.length);
-			map.put("title", lines[0]);
+			ArrayList<String> lines = resultString.get(i);
 			
-			map.put("link", lines[3]);
-			map.put("date", lines[2]);
+//			lines = resultString.get(i).split("\\$");
+//			System.out.println(lines[0]);
+//			for(String line : lines){
+//				System.out.println(line);
+//			}
+//			System.out.println("lines size : " + lines.length);
 			
-			map.put("description", lines[1]);
+			map.put("title", lines.get(0));
+			map.put("link", lines.get(3));
+			map.put("date", lines.get(2));
+			map.put("description", lines.get(1));
+			
 			System.out.println("Excel Input add");
 			data.add(map);
 			
@@ -246,6 +320,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		Main main = new Main();
-		main.start();
+		main.daumStart();
 	}
 }
---
+++ src/Manager.java
@@ -0,0 +1,14 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class Manager extends JFrame{
+	Manager(){
+		setTitle("Search Program");
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setLayout(new BorderLayout());
+		setSize(1000,100);
+		setVisible(true);
+	}
+
+}
